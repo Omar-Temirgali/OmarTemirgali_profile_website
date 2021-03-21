0784c7b215074c1864bc972a90db14539b447594
Omar-Temirgali <190103073@stu.sdu.edu.kz>
2021-03-22 00:14:28 +0600

    API Controller

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class ApiController extends Controller
+{
+    public function index(Request $request) {
+        $posts = Post::all();
+
+        return response($posts, 200);
+    }
+
+    public function get_post(Request $request) {
+        $post = Post::find($request->id);
+
+        if($post == null) {
+            return response(['message'=>'There is no such post'], 404);
+        }
+
+        return response($post, 200);
+    }
+}
